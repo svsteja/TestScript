992ebd0bc141d2a1769f166a330ebce56a8ae9ac
Teja <teja_siram@svs.teja2009@gmail.com>
2017-10-08 23:21:25 +0530

    Changes for build push to S3

--- src/main/java/com/testapplication/sampleapp/HelloController.java
+++ src/main/java/com/testapplication/sampleapp/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "HelloworldController";
+        return "NEW MODIFIED HelloworldController";
     }
     
 }
--- testscript.sh
+++ testscript.sh
@@ -1,3 +1,27 @@
-echo "First arg: $1"
-echo "Second arg: $2"
-echo "Build arg: ${GO_PIPELINE_COUNTER}"
+S3_BUCKET_NAME=prodbuilds
+S3_SUBFOLDER_NAME=SAMPLEAPP
+BUILD_NUMBER=${GO_PIPELINE_COUNTER}
+APPLICATION_JAR_NAME=sampleapp-0.0.1-SNAPSHOT.jar
+CURRENT_BUILD_S3LOCATION=s3://$S3_BUCKET_NAME/$S3_SUBFOLDER_NAME/CurrentBuild/$APPLICATION_JAR_NAME
+BUILD_NUMBER_S3LOCATION=s3://$S3_BUCKET_NAME/$S3_SUBFOLDER_NAME/$BUILD_NUMBER/$APPLICATION_JAR_NAME
+
+printf "\n"
+echo "=========================================="
+printf "\n"
+echo Uploading Build Number $BUILD_NUMBER to $CURRENT_BUILD_S3LOCATION
+printf "\n"
+aws s3 cp target/$APPLICATION_JAR_NAME $CURRENT_BUILD_S3LOCATION
+echo Uploading to current Build folder completed
+printf "\n"
+
+echo Uploading Build Number $BUILD_NUMBER to BUILD_NUMBER_S3LOCATION
+printf "\n"
+aws s3 cp target/$APPLICATION_JAR_NAME $BUILD_NUMBER_S3LOCATION
+echo Uploading to  Build Number folder completed
+printf "\n"
+
+
+
+
+
+
